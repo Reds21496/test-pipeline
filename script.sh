f25d338743817c935448cd486fbfa79ec01934de
Marco Vinicio Rojas Maroto <mrojasm@snapfinance.com>
2022-11-04 18:54:37 -0600

    Test

--- script.sh
+++ script.sh
@@ -3,6 +3,16 @@
 echo "Checking folder"
 ls -lart
 
+# Starts checking the format of the secret name
+echo "Checking secret name..."
+echo $SECRET_NAME
+
+# All symbols are replaced with underscores and all numbers are removed
+FORMATED_SN=$(echo $SECRET_NAME | sed -e 's/[^a-zA-Z0-9]/_/g' -e 's/[0-9]//g')
+
+# All letters are transformed to upper case
+FORMATED_SN=$(echo ${SECRET_NAME^^})
+
 echo "Secret name:"
 echo $SECRET_NAME
 
